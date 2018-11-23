a210e12c242c87551e867b501b50948ce85e43db
Mooradal <moorad2003@gmail.com>
2018-11-23 20:04:21 +0000

    Added viewsCount and Duration of the Video ▶.

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var input = document.getElementById('input-box');
 var search = document.getElementById('button-addon2');
-var searchURL = 'https://youtubeaccess-pyehgfcqld.now.sh/search';
-var redirect = 'https://youtubeaccess-pyehgfcqld.now.sh/download?id=';
-var thumbnail = 'https://youtubeaccess-pyehgfcqld.now.sh/thumbnail?id=';
+var searchURL = 'https://youtubeaccess-qmrrbznnbl.now.sh/search';
+var redirect = 'https://youtubeaccess-qmrrbznnbl.now.sh/download?id=';
+var thumbnail = 'https://youtubeaccess-qmrrbznnbl.now.sh/thumbnail?id=';
 var close = document.querySelector('.close');
 var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 
@@ -11,14 +11,12 @@ close.addEventListener('click', () => {
 });
 input.addEventListener('keyup', (e) => {
 	if (e.keyCode == 13) {
-		console.log(`Input : ${input.value}`);
 		sendSearch(input.value);
 	}
 });
 
 
 search.addEventListener('click', () => {
-	console.log(`Input : ${input.value}`);
 	sendSearch(input.value);
 });
 
@@ -55,20 +53,35 @@ function process(json) {
 		cardClone.childNodes[1].getElementsByClassName('channel-name')[0].innerText = data.channelTitle;
 		cardClone.childNodes[1].getElementsByClassName('video-description')[0].innerText = data.description;
 		cardClone.childNodes[1].getElementsByClassName('video-id')[0].innerText = 'ID: ' + data.videoId;
+		var duration = data.duration.match(/\d+(M|S|H)/gi);
+		duration = duration.join('').slice(0,-1).replace(/(H|M)/gi,':');
+		var splitted = duration.split(':');
+		for (var k = 0;k < splitted.length;k++) {splitted[k] = ('0'+splitted[k]).slice(-2);}
+		duration = splitted.join(':');
+		
+		cardClone.childNodes[1].getElementsByClassName('duration')[0].innerText = 'Duration: ' + duration;
+		var views = data.views;
+		if (views.length >= 10) {
+			views = views.slice(0,1)+'B';
+		} else if (views.length >= 7) {
+			views = views.slice(0,-6)+'M';
+		} else if (views.length >= 4) {
+			views = views.slice(0,-3)+'K';
+		}
+		cardClone.childNodes[1].getElementsByClassName('views')[0].innerText = views+' views';
+		
 		var date = data.publishedAt.slice(0,10);
 		date = `${date.slice(8,10)} ${months[date.slice(5,7)-1]} ${date.slice(0,4)}`;
 		cardClone.childNodes[1].getElementsByClassName('published-at')[0].innerText = 'Published At ' + date;
 		cardClone.removeAttribute('id');
 		cardClone.className = 'card mx-auto videos';
 		cardClone.style.display = 'block';
-		console.log(cardClone);
 		document.getElementById('main-container').appendChild(cardClone);
 	});
-	//console.log(cardClone);	
+	
 }
 
 function sendToDownload(element) {
 	var videoId = element.parentNode.querySelector('.video-id').innerText.slice(4);
-	console.log(videoId);
 	window.open(redirect+videoId);
 }
